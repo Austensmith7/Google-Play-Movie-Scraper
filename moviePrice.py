fdbf33a9493f791493b376aa8646c888291a02ff
Austen Smith <Austensmith7@gmail.com>
2019-12-24 19:01:20 -0600

    Add files via upload

---
+++ moviePrice.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 19:18:23 2019
+
+@author: auste
+"""
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+URL = "http://www.values.com/inspirational-quotes"
+r = requests.get(URL)
+
+soup = BeautifulSoup(r.content, 'html5lib')
+
+quotes = []  # a list to store quotes 
+
+table = soup.find('div', attrs={'id': 'container'})
+
+for row in table.findAll('div', attrs={'class': 'quote'}):
+    quote = {}
+    quote['theme'] = row.h5.text
+    quote['url'] = row.a['href']
+    quote['img'] = row.img['src']
+    quote['lines'] = row.h6.text
+    quote['author'] = row.p.text
+    quotes.append(quote)
+
+filename = 'inspirational_quotes.csv'
+with open(filename, 'wb') as f:
+    w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
+    w.writeheader()
+    for quote in quotes:
+        w.writerow(quote)
+
